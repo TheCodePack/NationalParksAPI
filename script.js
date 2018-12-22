9bb79d72767b63f32a844db164659c44deab31e8
Adam Katt <ak120908@gmail.com>
2018-12-22 12:08:53 -0500

    added JS functions

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
 const apiKey = 'a8HjUpjp6WFsYACczuSrhpPf8ExZDsXKI8VdztdL'
 
 const searchURL = 'https://developer.nps.gov/api/v1/parks'
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function displayParks(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+}
+
